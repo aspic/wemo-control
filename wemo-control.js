e53364a13be262112c3d7731e7864d8316d7328f
Kjetil Mehl <kjetil@mehl.no>
2016-01-24 17:26:05 +0100

    Reloads devices on toggle to fetch newest state

--- wemo-control.js
+++ wemo-control.js
@@ -1,5 +1,5 @@
 /**
- * Based off https://github.com/timonreinhard/wemo-client
+ * https://github.com/timonreinhard/wemo-client
  */
 var Wemo = require('wemo-client');
 var wemo = new Wemo();
@@ -9,27 +9,47 @@ var endDevices = [];
 exports.init = function() {
     wemo.discover(function(deviceInfo) {
         client = wemo.client(deviceInfo);
-        // TODO: reload?
-        client.getEndDevices(function(err, data){
-            endDevices = data;
-        });
+        exports.reload();
     });
 };
+
+exports.reload = function(cb) {
+    client.getEndDevices(function(err, data){
+        endDevices = data;
+        if(cb) {
+            cb(data);
+        }
+    });
+}
 
 exports.endDevices = function() {
     return endDevices;
 }
 
-exports.toggle = function(deviceId, action) {
+exports.toggle = function(deviceId, action, cb) {
     var actionId = action === "off" ? 0 : 1;
     
     client.setDeviceStatus(deviceId, 10006, actionId, function(err, resp) {
-        console.log(resp);
+        exports.reload(function(devices) {
+            var device = filterDevice(deviceId, devices);
+            cb(device);
+        });
     });
 };
     
 exports.dim = function(deviceId, value, time) {
     client.setDeviceStatus(deviceId, 10008, value + ":" + time, function(err, resp) {
-        console.log(resp);
+        // TODO
     });
 };
+
+function filterDevice(deviceId, devices) {
+    var device;
+    for(var i = 0; i < devices.length; i++) {
+        if(deviceId === devices[i].deviceId) {
+            device = devices[i];
+            break;
+        }
+    }
+    return device;
+}
