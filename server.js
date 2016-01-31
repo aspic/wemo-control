58a66852b710cb31c31bdc4148e0c43b6283fbce
Kjetil Mehl <kjetil@mehl.no>
2016-01-31 17:59:40 +0100

    Port is configurable

--- server.js
+++ server.js
@@ -64,9 +64,14 @@ app.get('*', function (req, res) {
     res.sendFile(path.join(__dirname, 'build/index.html'));
 });
 
-app.listen(3000, function (err) {
+var port = 8080;
+if(config.host) {
+    port = config.host.port;
+}
+
+app.listen(port, function (err) {
   if (err) {
     return console.error(err);
   }
-  console.log('Listening at http://localhost:3000');
+  console.log('Listening on http://localhost:' + port);
 });
